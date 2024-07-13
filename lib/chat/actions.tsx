93a7951982fe4dc6403f6bf68bfe53a9fb1d84cb
Carlos Avila <cavilart21@gmail.com>
2024-07-13 17:21:44 +0200

    update actions saved this is WIP

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -1,6 +1,16 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  createStreamableUI,
+  createStreamableValue,
+  getMutableAIState,
+} from "ai/rsc";
+import { Spinner } from "@nextui-org/spinner";
+import { streamText } from "ai";
+
+import { nanoid } from "../utils";
 
 import { isRegistered } from "./registered";
+import { google } from "@ai-sdk/google";
 
 const genAI = new GoogleGenerativeAI(
   process.env.GOOGLE_GENERATIVE_AI_API_KEY ?? ""
@@ -10,4 +20,43 @@ async function submitUserMessage(content: string) {
   "use server";
 
   await isRegistered();
+
+  const aiState = getMutableAIState();
+
+  aiState.update({
+    ...aiState.get(),
+    id: nanoid(),
+    role: "user",
+    content: `${aiState.get().interactions.join("\n\n")}\n\n${content}`,
+  });
+
+  const history = aiState
+    .get()
+    .messages.map((message: { role: any; content: any }) => ({
+      role: message.role,
+      content: message.content,
+    }));
+
+  const textStream = createStreamableValue("");
+  const spinnerStream = createStreamableUI(<Spinner />);
+  const messageStream = createStreamableUI(null);
+  const uiStream = createStreamableUI();
+
+  async function generateAIResponse() {
+    try {
+      const result = await streamText({
+        model: google("models/gemini-1.5-flash-latest"),
+        temperature: 0.5,
+        tools: {},
+        system: `
+          You are a friendly assistance to get user requirements and transform them into actionable user stories.
+          You can help users to prioritize tasks and keep their team on the same page.
+          If the user does not provide clear details, you can ask questions to get more information.
+          get as much informed about the project as possible.
+          before generating the user stories, you can ask the user to confirm if all the details are included.
+        `,
+        messages: history,
+      });
+    } catch (error) {}
+  }
 }
