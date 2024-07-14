7b5d42ceb9d28402ed8ba274375e0b9ff842bb8c
Carlos Avila <cavilart21@gmail.com>
2024-07-14 17:13:23 +0200

    adding key board support

--- components/Chat/ChatPanel.tsx
+++ components/Chat/ChatPanel.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Textarea } from "@nextui-org/react";
+import { Button, Textarea } from "@nextui-org/react";
 import { useState } from "react";
 import { IoMdSend } from "react-icons/io";
 
@@ -12,6 +12,15 @@ export const ChatPanel = () => {
         placeholder="Describe your project to us"
         value={message}
         onChange={(evt) => setMessage(evt.target.value)}
+        onKeyUp={(evt) => {
+          if (
+            (evt.key === "Enter" && evt.ctrlKey) ||
+            (evt.metaKey && evt.key === "Enter")
+          ) {
+            console.log("Send message:", message);
+            setMessage("");
+          }
+        }}
       />
       <Button isIconOnly aria-label="send" className="bg-transparent text-xl">
         <IoMdSend />
