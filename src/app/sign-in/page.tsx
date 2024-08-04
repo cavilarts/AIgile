9566f9a04aa6db57b236d1c90aab65efb53bb948
Carlos Avila <cavilart21@gmail.com>
2024-08-04 09:58:58 +0200

    update sign in stzles and remove custom login

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -5,11 +5,20 @@ import { Divider } from "@nextui-org/react";
 
 export default function Login() {
   return (
-    <section>
-      <GoogleSignInButton />
-      <Divider />
-      <span>or</span>
-      <AuthForm />
+    <section className="flex flex-col items-center justify-center w-full h-screen">
+      <h1 className="text-3xl font-extrabold sm:text-5xl text-center">
+        Welcome to AIgile
+      </h1>
+      <p className="mt-4 max-w-lg sm:text-xl/relaxed text-secondary-900 text-center">
+        Effortlessly transform requirements into actionable user stories,
+        prioritize tasks, and keep your team on the same page.
+      </p>
+      <p className="mt-4 max-w-lg sm:text-xl/relaxed text-secondary-900 text-center">
+        Please sign in to continue.
+      </p>
+      <div className="max-w-xs">
+        <GoogleSignInButton />
+      </div>
     </section>
   );
 }
